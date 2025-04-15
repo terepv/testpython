5aeb94c36e63f3e3a43dfdd7deb3590703df8fb8
terepv <teresa.pegna@gmail.com>
2025-04-14 21:32:23 -0400

    testsssss

--- demo-pipeline/myapp/tests.py
+++ demo-pipeline/myapp/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+# Test del endpoint ping
+class PingTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_endpoint_ping_responde_ping_pong(self):
+        response = self.client.get('/ping/')
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {'ping': 'pong'})
